4433a747afcfa6a2b5c211bc0b94529443cee322
kenjima3301 <danhtran3301@gmail.com>
2026-05-18 10:10:13 +0700

    feat: unlock module processor-core

--- infrastructure/envs/dev/main.tf
+++ infrastructure/envs/dev/main.tf
@@ -13,12 +13,12 @@ module "upload_trigger" {
   env    = "dev"
 }
 
-# module "processing_core" {
-#   source              = "../../modules/processing_core"
-#   env                 = "dev"
-#   s3_bucket_arn       = module.upload_trigger.s3_bucket_arn
-#   sqs_queue_arn       = module.upload_trigger.sqs_queue_arn
-#   dynamodb_table_arn  = module.my_database.table_arn
-#   dynamodb_table_name = module.my_database.table_name
-#   ecr_image_uri       = "${module.container_registry.repository_url}:latest"
-# }
+module "processing_core" {
+  source              = "../../modules/processing_core"
+  env                 = "dev"
+  s3_bucket_arn       = module.upload_trigger.s3_bucket_arn
+  sqs_queue_arn       = module.upload_trigger.sqs_queue_arn
+  dynamodb_table_arn  = module.my_database.table_arn
+  dynamodb_table_name = module.my_database.table_name
+  ecr_image_uri       = "${module.container_registry.repository_url}:${var.image_tag}"
+}
---
+++ infrastructure/envs/dev/variables.tf
@@ -0,0 +1,5 @@
+variable "image_tag" {
+  description = "Tag (Short SHA) of Docker image in ECR"
+  type        = string
+  default     = "latest"
+}
