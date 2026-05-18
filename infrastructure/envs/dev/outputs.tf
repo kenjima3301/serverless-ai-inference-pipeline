cb41f2793b4838db6386a8d92cbf4e8bd8e6d62e
kenjima3301 <danhtran3301@gmail.com>
2026-05-18 17:05:39 +0700

    trigger: force deploy infra

--- infrastructure/envs/dev/outputs.tf
+++ infrastructure/envs/dev/outputs.tf
@@ -2,3 +2,20 @@ output "client_api_url" {
   description = "Link API Gateway for Client to upload/get result"
   value       = module.client_api.api_endpoint
 }
+
+output "dynamodb_table_name" {
+  description = "DynamoDB table stores results"
+  value       = module.my_database.table_name
+}
+
+output "s3_upload_bucket" {
+  description = "S3 Bucket stores raw images"
+  value       = module.upload_trigger.s3_bucket_name
+}
+
+output "ecr_repository_url" {
+  value = module.container_registry.repository_url
+}
+output "aws_console_dynamodb_link" {
+  value = "https://ap-southeast-1.console.aws.amazon.com/dynamodbv2/home?region=ap-southeast-1#item-explorer?table=${module.my_database.table_name}"
+}
